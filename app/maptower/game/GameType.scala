6f7666b737699ebbf3eb608f77edb5821a274402
Kyle Unverferth <kyle@kyleu.com>
2012-03-02 02:06:08 -0500

    More conversion cleanup. I love writing parsers.

--- app/maptower/game/GameType.scala
+++ app/maptower/game/GameType.scala
@@ -5,7 +5,7 @@ import maptower.map.Point
 object GameType {
   val types = Map(
     "atlanta" -> new GameType("atlanta", "Downtown Atlanta", new Point(-84.3856, 33.7612), 16),
-    "eugene" -> new GameType("eugene", "Eugene, Oregon", new Point(-123.086667, 44.051944), 16))
+    "eugene" -> new GameType("eugene", "Eugene, Oregon", new Point(-123.086667, 44.051944), 14))
 }
 
 case class GameType(code: String, name: String, initialCenter: Point, initialZoom: Int)
