90b5574ac353cd331c50a9c4c0ddc23cb1d50fdb
Kyle Unverferth <kyle@kyleu.com>
2012-11-28 17:16:00 -0500

    Removing json4s

--- project/Build.scala
+++ project/Build.scala
@@ -14,11 +14,12 @@ object ApplicationBuild extends Build {
     "postgresql" % "postgresql" % "9.1-901-1.jdbc4",
     "org.scalaquery" % "scalaquery_2.9.0-1" % "0.9.5",
     "org.mongodb" % "casbah_2.9.1" % "2.4.0",
-    "org.scala-tools.testing" %% "scalacheck" % "1.9",
-    "org.json4s" %% "json4s-native" % "3.0.0"
+    "org.scala-tools.testing" %% "scalacheck" % "1.9"
   )
 
   println("Reloadinating!")
 
-  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings()
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+      
+  )
 }
